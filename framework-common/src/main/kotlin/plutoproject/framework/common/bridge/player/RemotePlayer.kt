373ac7ab707bdbf579f1ca06b8034a2885cfa696
nostalfinals <yuu8583@proton.me>
2025-03-21 20:40:31 +0800

    refactor(player): 更新玩家操作处理逻辑
    - 在 BackendLocalPlayer 和 ProxyLocalPlayer 中，将玩家操作的返回类型改为 ResultWrapper<Unit>- 在 RemotePlayer 中，添加 wrapResultWithoutValue 方法统一处理无返回值的操作
    - 更新 PlayerOperationHandler 中的处理逻辑，使用新的 ResultWrapper 结构
    - 移除不必要的 warn 调用，改为直接抛出错误

--- framework-common/src/main/kotlin/plutoproject/framework/common/bridge/player/RemotePlayer.kt
+++ framework-common/src/main/kotlin/plutoproject/framework/common/bridge/player/RemotePlayer.kt
@@ -4,25 +4,32 @@ import net.kyori.adventure.sound.Sound
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.minimessage.MiniMessage
 import net.kyori.adventure.title.Title
+import plutoproject.framework.common.api.bridge.ResultWrapper
+import plutoproject.framework.common.bridge.exception.PlayerOperationResultWrapper
 import plutoproject.framework.proto.bridge.BridgeRpcOuterClass.PlayerOperation
 import plutoproject.framework.proto.bridge.BridgeRpcOuterClass.PlayerOperationResult
+import plutoproject.framework.proto.bridge.BridgeRpcOuterClass.PlayerOperationResult.StatusCase.OK
 import plutoproject.framework.proto.bridge.playerOperation
 import plutoproject.framework.proto.bridge.soundInfo
 import plutoproject.framework.proto.bridge.titleInfo
 import java.util.*
 
 abstract class RemotePlayer : InternalPlayer() {
-    override suspend fun sendMessage(message: Component) {
-        operatePlayer(playerOperation {
+    protected fun wrapResultWithoutValue(result: PlayerOperationResult): ResultWrapper<Unit> =
+        PlayerOperationResultWrapper(Unit, result.statusCase!!, name, server.id, result.statusCase!! != OK)
+
+    override suspend fun sendMessage(message: Component): ResultWrapper<Unit> {
+        val result = operatePlayer(playerOperation {
             id = UUID.randomUUID().toString()
             executor = server.id
             playerUuid = uniqueId.toString()
             sendMessage = MiniMessage.miniMessage().serialize(message)
         })
+        return wrapResultWithoutValue(result)
     }
 
-    override suspend fun showTitle(title: Title) {
-        operatePlayer(playerOperation {
+    override suspend fun showTitle(title: Title): ResultWrapper<Unit> {
+        val result = operatePlayer(playerOperation {
             id = UUID.randomUUID().toString()
             executor = server.id
             playerUuid = uniqueId.toString()
@@ -35,10 +42,11 @@ abstract class RemotePlayer : InternalPlayer() {
                 subTitle = MiniMessage.miniMessage().serialize(title.subtitle())
             }
         })
+        return wrapResultWithoutValue(result)
     }
 
-    override suspend fun playSound(sound: Sound) {
-        operatePlayer(playerOperation {
+    override suspend fun playSound(sound: Sound): ResultWrapper<Unit> {
+        val result = operatePlayer(playerOperation {
             id = UUID.randomUUID().toString()
             executor = server.id
             playerUuid = uniqueId.toString()
@@ -49,6 +57,7 @@ abstract class RemotePlayer : InternalPlayer() {
                 pitch = sound.pitch()
             }
         })
+        return wrapResultWithoutValue(result)
     }
 
     abstract suspend fun operatePlayer(request: PlayerOperation): PlayerOperationResult
