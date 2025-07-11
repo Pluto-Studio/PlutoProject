cee9397e43f635065225a54cd77dca45d08f8453
nostalfinals <yuu8583@proton.me>
2025-07-11 14:11:21 +0800

    refactor: 移除测试指令

--- framework-paper/src/main/kotlin/plutoproject/framework/paper/Module.kt
+++ framework-paper/src/main/kotlin/plutoproject/framework/paper/Module.kt
@@ -6,7 +6,6 @@ import plutoproject.framework.common.api.rpc.RpcClient
 import plutoproject.framework.common.databasepersist.InternalDatabasePersist
 import plutoproject.framework.common.util.inject.Koin
 import plutoproject.framework.paper.api.interactive.GuiManager
-import plutoproject.framework.paper.databasepersist.TestCommand
 import plutoproject.framework.paper.interactive.GuiListener
 import plutoproject.framework.paper.interactive.commands.InteractiveCommand
 import plutoproject.framework.paper.interactive.inventory.InventoryListener
@@ -42,7 +41,6 @@ private fun registerListeners() = server.pluginManager.apply {
 private fun registerCommands() {
     AnnotationParser.apply {
         parse(InteractiveCommand)
-        parse(TestCommand)
     }
 }
 
