64a3b5c9794f91bff5d8fe04b6b5817de760e088
nostalfinals <yuu8583@proton.me>
2025-07-13 17:51:36 +0800

    fix: 未编码 MongoDB 链接地址中的特殊字符

--- framework-common/src/main/kotlin/plutoproject/framework/common/provider/ProviderImpl.kt
+++ framework-common/src/main/kotlin/plutoproject/framework/common/provider/ProviderImpl.kt
@@ -11,6 +11,8 @@ import org.koin.core.component.inject
 import plutoproject.framework.common.api.provider.Provider
 import plutoproject.framework.common.config.ProviderConfig
 import plutoproject.framework.common.util.getFrameworkModuleDataFolder
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
 
 class ProviderImpl : Provider, KoinComponent {
     private val config by inject<ProviderConfig>()
@@ -20,12 +22,15 @@ class ProviderImpl : Provider, KoinComponent {
 
     init {
         val mongoConfig = config.mongo
+        val encodedUsername = URLEncoder.encode(mongoConfig.username, StandardCharsets.UTF_8.toString())
+        val encodedPassword = URLEncoder.encode(mongoConfig.password, StandardCharsets.UTF_8.toString())
         val mongoSettings = MongoClientSettings.builder()
-            .applyConnectionString(ConnectionString("mongodb://${mongoConfig.username}:${mongoConfig.password}@${mongoConfig.host}:${mongoConfig.port}/${mongoConfig.database}"))
+            .applyConnectionString(ConnectionString("mongodb://${encodedUsername}:${encodedPassword}@${mongoConfig.host}:${mongoConfig.port}/${mongoConfig.database}"))
             .uuidRepresentation(UuidRepresentation.STANDARD)
             .build()
         mongoClient = MongoClient.create(mongoSettings)
         defaultMongoDatabase = mongoClient.getDatabase(mongoConfig.database)
+
         val geoIpConfig = config.geoIp
         val dbFile = getFrameworkModuleDataFolder("provider").resolve(geoIpConfig.database)
         check(dbFile.exists()) { "GeoIP database file not found" }
