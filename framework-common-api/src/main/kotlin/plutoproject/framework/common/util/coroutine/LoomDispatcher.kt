f6643bd915f75bac8d2b41a79e3960974ea193eb
nostalfinals <yuu8583@proton.me>
2025-08-05 15:07:42 +0800

    docs: 添加 PluginScope 的描述

--- framework-common-api/src/main/kotlin/plutoproject/framework/common/util/coroutine/LoomDispatcher.kt
+++ framework-common-api/src/main/kotlin/plutoproject/framework/common/util/coroutine/LoomDispatcher.kt
@@ -5,8 +5,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.asCoroutineDispatcher
 import java.util.concurrent.Executors
 
-private val loomExecutor = Executors.newVirtualThreadPerTaskExecutor()
-private val loomDispatcher = loomExecutor.asCoroutineDispatcher()
+private val loomDispatcher = Executors.newVirtualThreadPerTaskExecutor().asCoroutineDispatcher()
 
 /**
  * 基于虚拟线程的 [CoroutineDispatcher]，适合用于 IO 任务。
