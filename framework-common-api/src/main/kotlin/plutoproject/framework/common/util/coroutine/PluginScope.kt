420fcae79308a14c34dad551649eb7ab98d3f5aa
nostalfinals <yuu8583@proton.me>
2025-08-05 15:07:42 +0800

    docs: 添加 PluginScope 的描述

--- framework-common-api/src/main/kotlin/plutoproject/framework/common/util/coroutine/PluginScope.kt
+++ framework-common-api/src/main/kotlin/plutoproject/framework/common/util/coroutine/PluginScope.kt
@@ -1,6 +1,12 @@
 package plutoproject.framework.common.util.coroutine
 
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.SupervisorJob
 
+/**
+ * 插件内部使用的 [CoroutineScope]，会在插件关闭时一并关闭。
+ *
+ * 请勿使用 [GlobalScope] 来开启协程，因为使用它运行的任务无法随着插件卸载一并被取消。
+ */
 object PluginScope : CoroutineScope by CoroutineScope(SupervisorJob())
