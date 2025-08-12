c6ecc3449f8276e460142062a7fe4bc6b42f1fd6
nostalfinals <yuu8583@proton.me>
2025-08-12 14:36:52 +0800

    feature: 新增两个 2025 活动模块

---
+++ feature-paper/src/main/kotlin/plutoproject/feature/paper/event_2025_itemrace/ItemRaceEventFeature.kt
@@ -0,0 +1,12 @@
+package plutoproject.feature.paper.event_2025_itemrace
+
+import plutoproject.framework.common.api.feature.Platform
+import plutoproject.framework.common.api.feature.annotation.Feature
+import plutoproject.framework.paper.api.feature.PaperFeature
+
+@Feature(
+    id = "event_2025_itemrace",
+    platform = Platform.PAPER
+)
+class ItemRaceEventFeature : PaperFeature() {
+}
