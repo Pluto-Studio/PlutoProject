f8987b71bd88064f78ce51ac9e4837f16f538271
Developer <dev@example.com>
2026-01-30 22:33:44 +0800

    feat: 实现部分访客隐藏逻辑

--- feature-paper/src/main/kotlin/plutoproject/feature/paper/whitelist_v2/VisitorListener.kt
+++ feature-paper/src/main/kotlin/plutoproject/feature/paper/whitelist_v2/VisitorListener.kt
@@ -4,6 +4,7 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.bukkit.GameMode
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
@@ -13,6 +14,8 @@ import org.koin.core.component.get
 import plutoproject.feature.common.api.whitelist_v2.Whitelist
 import plutoproject.feature.common.whitelist_v2.WhitelistImpl
 import plutoproject.framework.common.util.coroutine.PluginScope
+import plutoproject.framework.paper.util.plugin
+import plutoproject.framework.paper.util.server
 import java.util.*
 import java.util.concurrent.ConcurrentHashMap
 import kotlin.time.Duration.Companion.seconds
@@ -42,6 +45,7 @@ object VisitorListener : Listener, KoinComponent {
         if (whitelist.isKnownVisitor(player.uniqueId)) {
             pendingVisitors.remove(player.uniqueId)?.cancel()
             player.gameMode = GameMode.SPECTATOR
+            hideVisitorFromAllPlayers(player)
         }
     }
 
@@ -51,6 +55,29 @@ object VisitorListener : Listener, KoinComponent {
         if (whitelist.isKnownVisitor(player.uniqueId)) {
             whitelist.removeKnownVisitor(player.uniqueId)
             pendingVisitors.remove(player.uniqueId)?.cancel()
+        }
+    }
+
+    private fun hideVisitorFromAllPlayers(visitor: Player) {
+        server.onlinePlayers.forEach { otherPlayer ->
+            if (otherPlayer.uniqueId != visitor.uniqueId) {
+                otherPlayer.hidePlayer(plugin, visitor)
+                if (whitelist.isKnownVisitor(otherPlayer.uniqueId)) {
+                    visitor.hidePlayer(plugin, otherPlayer)
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    fun onNonVisitorPlayerJoin(event: PlayerJoinEvent) {
+        val player = event.player
+        if (!whitelist.isKnownVisitor(player.uniqueId)) {
+            server.onlinePlayers.forEach { onlinePlayer ->
+                if (whitelist.isKnownVisitor(onlinePlayer.uniqueId)) {
+                    player.hidePlayer(plugin, onlinePlayer)
+                }
+            }
         }
     }
 }
--- feature-velocity/src/main/kotlin/plutoproject/feature/velocity/joinQuitMessage/JoinQuitMessageFeature.kt
+++ feature-velocity/src/main/kotlin/plutoproject/feature/velocity/joinQuitMessage/JoinQuitMessageFeature.kt
@@ -3,6 +3,7 @@ package plutoproject.feature.velocity.joinQuitMessage
 import com.github.shynixn.mccoroutine.velocity.registerSuspend
 import org.koin.dsl.module
 import plutoproject.framework.common.api.feature.Platform
+import plutoproject.framework.common.api.feature.annotation.Dependency
 import plutoproject.framework.common.api.feature.annotation.Feature
 import plutoproject.framework.common.util.config.loadConfig
 import plutoproject.framework.common.util.inject.configureKoin
@@ -13,6 +14,7 @@ import plutoproject.framework.velocity.util.server
 @Feature(
     id = "join_quit_message",
     platform = Platform.VELOCITY,
+    dependencies = [Dependency("whitelist_v2", required = false)]
 )
 @Suppress("UNUSED")
 class JoinQuitMessageFeature : VelocityFeature() {
