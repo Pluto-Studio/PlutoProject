946482fed8789ed60704f2f9654cd77e0a5ac9c0
nostalfinals <yuu8583@proton.me>
2026-03-14 01:36:51 +0800

    fix: 集合名称按服务器名称区分

--- feature/gallery/adapter-common/src/main/kotlin/plutoproject/feature/gallery/adapter/common/Di.kt
+++ feature/gallery/adapter-common/src/main/kotlin/plutoproject/feature/gallery/adapter/common/Di.kt
@@ -1,54 +1,35 @@
 package plutoproject.feature.gallery.adapter.common
 
 import com.mongodb.kotlin.client.coroutine.MongoCollection
-import org.koin.core.qualifier.named
 import org.koin.core.module.dsl.singleOf
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import plutoproject.feature.gallery.core.ImageDataEntryRepository
-import plutoproject.feature.gallery.core.ImageManager
 import plutoproject.feature.gallery.core.ImageRepository
 import plutoproject.feature.gallery.core.decode.decoder.ImageDecoder
 import plutoproject.feature.gallery.core.decode.decoder.defaultGifDecoder
 import plutoproject.feature.gallery.core.decode.decoder.defaultStaticImageDecoder
-import plutoproject.feature.gallery.core.render.AnimatedImageRenderer
-import plutoproject.feature.gallery.core.render.DefaultAnimatedImageRenderer
-import plutoproject.feature.gallery.core.render.DefaultStaticImageRenderer
-import plutoproject.feature.gallery.core.render.StaticImageRenderer
-import plutoproject.feature.gallery.core.render.defaultFrameSampler
+import plutoproject.feature.gallery.core.render.*
 import plutoproject.feature.gallery.core.render.mapcolor.defaultAlphaCompositor
 import plutoproject.feature.gallery.core.render.mapcolor.defaultMapColorQuantizer
-import plutoproject.feature.gallery.core.usecase.ChangeImageOwnerNameUseCase
-import plutoproject.feature.gallery.core.usecase.CreateImageDataEntryUseCase
-import plutoproject.feature.gallery.core.usecase.CreateImageUseCase
-import plutoproject.feature.gallery.core.usecase.DeleteImageDataEntryUseCase
-import plutoproject.feature.gallery.core.usecase.DeleteImageUseCase
-import plutoproject.feature.gallery.core.usecase.DecodeImageUseCase
-import plutoproject.feature.gallery.core.usecase.GetImageDataEntryUseCase
-import plutoproject.feature.gallery.core.usecase.GetImageUseCase
-import plutoproject.feature.gallery.core.usecase.LookupImageByOwnerUseCase
-import plutoproject.feature.gallery.core.usecase.RenameImageUseCase
-import plutoproject.feature.gallery.core.usecase.RenderAnimatedImageUseCase
-import plutoproject.feature.gallery.core.usecase.RenderStaticImageUseCase
-import plutoproject.feature.gallery.core.usecase.ReplaceImageDataEntryUseCase
+import plutoproject.feature.gallery.core.usecase.*
 import plutoproject.feature.gallery.infra.mongo.MongoImageDataEntryRepository
 import plutoproject.feature.gallery.infra.mongo.MongoImageRepository
 import plutoproject.feature.gallery.infra.mongo.model.ImageDataEntryDocument
 import plutoproject.feature.gallery.infra.mongo.model.ImageDocument
 import plutoproject.framework.common.api.connection.MongoConnection
 import plutoproject.framework.common.api.connection.getCollection
-import java.time.Clock
+import plutoproject.framework.common.util.serverName
 
 private const val GALLERY_PREFIX = "gallery_"
-private const val IMAGE_COLLECTION = "images"
-private const val IMAGE_DATA_ENTRY_COLLECTION = "image_data_entries"
+private const val IMAGE_COLLECTION = "image"
+private const val IMAGE_DATA_ENTRY_COLLECTION = "image_data"
 
 private inline fun <reified T : Any> getCollection(name: String): MongoCollection<T> {
-    return MongoConnection.getCollection("$GALLERY_PREFIX$name")
+    return MongoConnection.getCollection("$GALLERY_PREFIX${serverName}_$name")
 }
 
 val commonModule = module {
-    singleOf(::ImageManager)
-
     single<ImageRepository> {
         MongoImageRepository(getCollection<ImageDocument>(IMAGE_COLLECTION))
     }
