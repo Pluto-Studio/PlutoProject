03b2414f8fe435437a11ac954a560a57b7955c79
nostalfinals <yuu8583@proton.me>
2026-03-14 00:05:26 +0800

    refactor: 删掉没有用的 util 函数

--- feature/gallery/core/src/main/kotlin/plutoproject/feature/gallery/core/util/MinecraftUsernameValidator.kt
+++
@@ -1,7 +0,0 @@
-package plutoproject.feature.gallery.core.util
-
-private val MINECRAFT_USERNAME_REGEX = Regex("^[A-Za-z0-9_]{3,16}$")
-
-internal fun isValidOwnerName(name: String): Boolean {
-    return MINECRAFT_USERNAME_REGEX.matches(name)
-}
